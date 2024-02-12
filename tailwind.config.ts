e5631b01b6687e0e34d381ba5275b2fdd402f510
madsab <mads.barnes@hotmail.com>
2024-02-12 16:06:17 +0100

    add: inner shadow x direction

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        "inner-x": "inset 0px 0px 15px 10px rgba(0, 0, 0, 0.5);",
+      }
     },
   },
   plugins: [],
