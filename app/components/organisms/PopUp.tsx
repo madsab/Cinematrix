2212e2c8e76b330b572539f26a5b2f300e8367a3
madsab <mads.barnes@hotmail.com>
2024-02-15 12:21:20 +0100

    new: Popup component

--- app/components/organisms/PopUp.tsx
+++ app/components/organisms/PopUp.tsx
@@ -1,25 +1,49 @@
 import cn from "classnames";
-import React, { FC, PropsWithChildren } from "react";
+import { FC, useEffect, useRef, useState } from "react";
+import Button from "../atoms/Button";
 
 interface PopUpProps {
   children: React.ReactNode;
   className?: string;
+  open: boolean;
+  onClose: () => void;
 }
 
-const PopUp: FC<PopUpProps> = ({ children, className }) => {
+const PopUp: FC<PopUpProps> = ({ children, className, open, onClose }) => {
+  const ref = useRef<any>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (open && ref.current && !ref.current.contains(event.target)) {
+        onClose();
+        console.log("click outside");
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [ref, open, onClose]);
+
   return (
     <div
+      ref={ref}
       className={cn(
-        "absolute w-1/2 flex flex-col items-center p-6 rounded-md z-50 bg-slate-600",
+        "absolute flex flex-col items-center p-6 rounded-md z-50 bg-black border-2 border-slate-900 shadow-md",
         className
+          ? className
+          : "w-full top-1/4 sm:w-1/2 sm:left-1/4 sm:top-1/4",
+        !open && "hidden"
       )}
     >
       {children}
-      <div className="w-full flex justify-end">
-        {/* Insert button component here */}
-        <button className="bg-purple-700 p-2 rounded-md">
-          Instert Button here
-        </button>
+      <div className="w-full flex justify-end space-x-3">
+        <Button onClick={onClose} type="cancel">
+          Cancel
+        </Button>
+        <Button onClick={onClose} type="confirm">
+          Confirm
+        </Button>
       </div>
     </div>
   );
