fc93f55bcb52b2a727942520d863ab68a6802d93
madsab <mads.barnes@hotmail.com>
2024-02-19 10:56:20 +0100

    new: db get movies

---
+++ app/api/movies/route.ts
@@ -0,0 +1,19 @@
+import { db } from "@/firebase/config";
+import { collection, query, where, limit, getDocs } from "firebase/firestore";
+import { NextRequest } from "next/server";
+
+export async function GET() {
+    try {
+        const movieCollection = collection(db, "movies")
+        const q = query(movieCollection, where("year", ">=", 2010), limit(30))
+        const data = await getDocs(q)
+        const movies = data.docs.map((doc) => {
+            const movieData = doc.data();
+            movieData.id = doc.id;
+            return movieData;
+        });
+        return new Response(JSON.stringify(movies));
+    } catch (error) {
+        return new Response("Error", { status: 500 })
+    }
+}
