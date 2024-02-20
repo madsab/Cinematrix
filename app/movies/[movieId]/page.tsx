e0c96954176b8339d3b7c5c8fe6d35cdacded0e0
Sindy <sindys@stud.ntnu.no>
2024-02-20 14:32:43 +0100

    Changed the style to tailwind

--- app/movies/[movieId]/page.tsx
+++ app/movies/[movieId]/page.tsx
@@ -2,14 +2,13 @@
 import React, { useState, useEffect } from "react";
 import Annika from "../../assets/images/Anikka.jpeg";
 import { MovieCardProps } from "../../components/organisms/MovieCard";
-import "./page.css";
 
 const dummyMovies: MovieCardProps[] = [
   {
     id: 1,
     title: "The Shawshank Redemption",
     description:
-      "Chronicles the experiences of a Chronicles the experiences of a formerly successful banker as a prisoner in the Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red.",
+      "Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red.",
     image: Annika,
     rating: 5,
     actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
@@ -19,33 +18,40 @@ const dummyMovies: MovieCardProps[] = [
 const MoviePage = ({ params }: { params: { movieId: string } }) => {
   const [movie, setMovie] = useState<MovieCardProps | null>(null);
   useEffect(() => {
-    const foundMovie = dummyMovies.find((m) => m.id === m.id);
+    const foundMovie = dummyMovies.find(
+      (m) => m.id === parseInt(params.movieId)
+    );
     setMovie(foundMovie || null);
   }, [params.movieId]);
 
   if (!movie) {
-    return <div>Movie not found</div>;
+    return <div className="text-white">Movie not found</div>;
   }
 
   return (
-    <div className="page">
-      <div className="movielayout">
+    <div className="flex flex-col md:flex-row justify-center items-start bg-gradient-to-r from-black to-custom-red text-white overflow-hidden shadow-lg p-5 min-h-screen w-full">
+      <div className="flex justify-center md:justify-start md:items-center mb-4 md:mb-0">
         {movie.image && (
-          <div className="movie-image">
-            <img src={movie.image.src} alt={movie.title} />
-          </div>
+          <img
+            src={movie.image.src}
+            alt={movie.title}
+            className="rounded-lg max-w-s md:max-w-sm lg:max-w-md mr-8"
+          />
         )}
-        <div className="movieinfo">
-          <h1 className="title">{movie.title}</h1>
-          <p className="description">{movie.description}</p>
-          <p className="actors">
-            <b>Actors:</b> {movie.actors?.join(", ")}
-          </p>
-          <p className="rating">
-            {" "}
-            <b>Rating:</b> {movie.rating}/5
-          </p>
-        </div>
+      </div>
+      <div className="flex flex-col justify-start p-5">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-6 border-b border-gray-200 pb-2">
+          {movie.title}
+        </h1>
+        <p className="text-md md:text-lg lg:text-xl mb-4">
+          {movie.description}
+        </p>
+        <p className="mb-2">
+          <span className="font-bold">Actors:</span> {movie.actors?.join(", ")}
+        </p>
+        <p>
+          <span className="font-bold">Rating:</span> {movie.rating}/5
+        </p>
       </div>
     </div>
   );
