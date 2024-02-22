bddf7740a5ad9df75d63f4b570f2ce62988381da
niklasu <niklasu@smint.no>
2024-02-22 12:02:01 +0100

    GET in movies\route.ts added rating

--- app/api/users/[id]/movies/route.ts
+++ app/api/users/[id]/movies/route.ts
@@ -4,27 +4,57 @@ import { NextApiResponse } from "next";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest, params: {params: { id: string }}) {
-    const type = req.nextUrl.searchParams.get("type")
+    const fieldType = req.nextUrl.searchParams.get("fieldType") // Watched or Rated
+    const type = req.nextUrl.searchParams.get("type") // ID or nothing (nothing will return the whole movie object (or a list of them..)
+    const movieID = req.nextUrl.searchParams.get("movieID")
     const userMoviesDB = doc(db, "users", params.params.id)
     const movieDB = collection(db, "movies")
     const watchedMovies: any[] = []
     try{
-        const moviesWatched = (await getDoc(userMoviesDB)).get("moviesWatched") as []
-        if (type === "MoviesWatchedID") {
-            return new Response(JSON.stringify(moviesWatched))
-        }
-        //Had to add moviesWatched to an array to be able to use it in next query, or else moviesWatched would be empty.
-        moviesWatched.forEach((movie) => {
-            watchedMovies.push(movie)
-        })
-        const data = await getDocs(query(movieDB, where("imdbid", "in", watchedMovies)))
 
-        const movies = data.docs.map((doc) => {
-            const movieData = doc.data();
-            movieData.id = doc.id;
-            return movieData;
-        });
-        return new Response(JSON.stringify(movies))
+        if (fieldType === "Watched") {
+            const moviesWatched = (await getDoc(userMoviesDB)).get("moviesWatched") as []
+            if (type === "ID") {
+                return new Response(JSON.stringify(moviesWatched))
+            }
+            if (movieID !== null) {
+                const data = await getDocs(query(movieDB, where(movieID, "in", watchedMovies)))
+
+                const movies = data.docs.map((doc) => {
+                    const movieData = doc.data();
+                    movieData.id = doc.id;
+                    return movieData;
+                });
+                return new Response(JSON.stringify(movies))
+            }
+            //Had to add moviesWatched to an array to be able to use it in next query, or else moviesWatched would be empty.
+            moviesWatched.forEach((movie) => {
+                watchedMovies.push(movie)
+            })
+            const data = await getDocs(query(movieDB, where("imdbid", "in", watchedMovies)))
+
+            const movies = data.docs.map((doc) => {
+                const movieData = doc.data();
+                movieData.id = doc.id;
+                return movieData;
+            });
+
+        }
+
+        if (fieldType === "Rated") {
+            const moviesRated = (await getDoc(userMoviesDB)).get("moviesRated") as []
+
+            if (type === "ID") {
+                return new Response(JSON.stringify(moviesRated))
+            }
+            const ratedMovies: any[] = []
+            moviesRated.forEach( (movie) => {
+                ratedMovies.push(movie)
+            })
+        }
+
+
+
 
 
     } catch (error) {
@@ -36,22 +66,28 @@ export async function GET(req: NextRequest, params: {params: { id: string }}) {
 
 export async function POST(req: NextRequest, params: {params: { id: string }}, res: NextApiResponse) {
     const type = req.nextUrl.searchParams.get("type")
-
     const userDoc = doc(db, "users", params.params.id)
+
+
     if (type === "MovieWatched") {
         const { movieImdbId  } = await req.json()
         await updateDoc(userDoc, {
             moviesWatched: arrayUnion(movieImdbId),
         })
     }
-
+//movieRated is not stored as an Array, hence arrayUnion can't be used
     if (type === "MovieRatings") {
-        let map: any = new Map();
+        let mapPush: any = new Map();
         const { movieImdbId, rating } = await req.json()
-        map.set(movieImdbId, rating)
+        mapPush.set(movieImdbId, rating)
+
+        const field = (await getDoc(userDoc)).get("movieRated")
+        console.log(field)
+        const updatedData = {...field, ...mapPush}
         await updateDoc(userDoc, {
-            moviesRated: arrayUnion(map),
+            moviesRated: updatedData
         })
+
     }
     return res.status(201)
 }
