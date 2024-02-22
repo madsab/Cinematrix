575fc55cfb0ae4579944a96e9c57ac34f9ade79b
Sebastian Munthe <sebasmu@stud.ntnu.no>
2024-02-22 12:13:04 +0100

    Fetch movie from database

--- app/(main)/movies/[movieId]/page.tsx
+++ app/(main)/movies/[movieId]/page.tsx
@@ -6,28 +6,49 @@ import MovieButton from "../../../components/atoms/Moviebutton";
 import Stars from "@/app/components/atoms/Stars";
 import PopUp from "@/app/components/organisms/PopUp";
 import Button from "@/app/components/atoms/Button";
+import { Movie } from "../../../types/Movie";
+import { auth } from "@/firebase/config";
+import { redirect } from "next/navigation";
 
-const dummyMovies: MovieCardProps[] = [
-  {
-    id: 1,
-    title: "The Shawshank Redemption",
-    description:
-      "Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red.",
-    image: Annika,
-    rating: 5,
-    actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
-  },
-];
+
 
 const MoviePage = ({ params }: { params: { movieId: string } }) => {
-  const [movie, setMovie] = useState<MovieCardProps | null>(null);
+  const [movie, setMovie] = useState<Movie>();
   const [open, setOpen] = useState(false);
+  const [notLoggedIn, setNotLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const foundMovie = dummyMovies.find(
-      (m) => m.id === parseInt(params.movieId)
-    );
-    setMovie(foundMovie || null);
-  }, [params.movieId]);
+      const fetchMovies = async () => {
+        const res = await fetch("/api/movies", {
+          method: "GET",
+        });
+        const data = await res.json();
+        console.log(data);
+        const foundMovie = data.find(
+          (m: Movie) => m.id == params.movieId
+        );
+        setMovie(foundMovie);
+      };
+
+    if (notLoggedIn) {
+      redirect("/signin");
+    } else {
+      fetchMovies();
+    }
+
+  }, [notLoggedIn]);
+
+  auth.onAuthStateChanged((user) => {
+    setLoading(false);
+
+    if (user) {
+      setNotLoggedIn(false);
+    } else {
+      setNotLoggedIn(true);
+    }
+  });
+
   const closePopup = () => {
     setOpen(false);
   };
@@ -35,16 +56,16 @@ const MoviePage = ({ params }: { params: { movieId: string } }) => {
     setOpen(true);
   };
 
-  if (!movie) {
-    return <div className="text-white">Movie not found</div>;
-  }
-
   return (
-    <div className="flex flex-col md:flex-row justify-center items-start bg-gradient-to-r from-black to-[#801336] text-white overflow-hidden shadow-lg p-5 min-h-screen w-full">
+    <>
+    {movie == null ? 
+    (<div>Loading</div>) : (
+      <div>
+      <div className="flex flex-col md:flex-row justify-center items-start bg-gradient-to-r from-black to-[#801336] text-white overflow-hidden shadow-lg p-5 min-h-screen w-full">
       <div className="flex justify-center md:justify-start md:items-center mb-4 md:mb-0">
-        {movie.image && (
+        {movie && (
           <img
-            src={movie.image.src}
+            src={movie.big_image}
             alt={movie.title}
             className="rounded-lg max-w-s md:max-w-sm lg:max-w-md mr-8"
           />
@@ -53,16 +74,16 @@ const MoviePage = ({ params }: { params: { movieId: string } }) => {
       <div className="flex flex-col justify-start p-5">
         <MovieButton />
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-6 border-b border-gray-200 pb-2">
-          {movie.title}
+          {movie?.title}
         </h1>
         <p className="text-md md:text-lg lg:text-xl mb-4">
-          {movie.description}
+          {movie?.description}
         </p>
         <p className="mb-2">
-          <span className="font-bold">Actors:</span> {movie.actors?.join(", ")}
+          <span className="font-bold">Genres:</span> {movie?.genre?.join(", ")}
         </p>
         <p>
-          <span className="font-bold">Rating:</span> {movie.rating}/5
+          <span className="font-bold">Rating:</span> {movie?.rating}/10
         </p>
         <Button onClick={openPopup}> "Rate this movie:"</Button>
         <PopUp open={open} onClose={closePopup}>
@@ -70,6 +91,10 @@ const MoviePage = ({ params }: { params: { movieId: string } }) => {
         </PopUp>
       </div>
     </div>
+    </div>
+    )}
+    </>
+    
   );
 };
 
