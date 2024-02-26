54ac2e42421bfcd8cff783604c8aa49a34976cd2
madsab <mads.barnes@hotmail.com>
2024-02-26 20:42:50 +0100

    add: forwardRef for using Stars.tsx methods

--- app/components/atoms/Stars.tsx
+++ app/components/atoms/Stars.tsx
@@ -1,43 +1,58 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
-import React, { FC, useEffect, useState } from "react";
+import React, {
+  FC,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from "react";
 import cn from "classnames";
-import { set } from "firebase/database";
+import { or } from "firebase/firestore";
 
-interface StarsProps {
+export interface StarsProps {
   save: boolean;
   userId: string | null;
   movieImdbId: string | undefined;
-  reset?: boolean;
 }
 
-const Stars: FC<StarsProps> = ({
-  userId,
-  movieImdbId,
-  save = false,
-  reset,
-}) => {
-  const size = 30;
-  const [originalRating, setOriginalRating] = useState(0); // Todo replace with actual rating
-  const [currentRating, setCurrentRating] = useState(0);
-  const [clicked, setClicked] = useState(false);
+export type StarsRef = {
+  saveToDb: () => void;
+  resetRating: () => void;
+};
 
-  useEffect(() => {
-    const fetchUserRating = async () => {
-      const res = await fetch(
-        `/api/users/${userId}/movies?fieldType=Rated&movieID=${movieImdbId}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = await res.json();
-      setCurrentRating(data);
-    };
+const Stars = forwardRef<StarsRef, StarsProps>(
+  ({ userId, movieImdbId }: StarsProps, ref) => {
+    const size = 30;
+    const [originalRating, setOriginalRating] = useState(0); // Todo replace with actual rating
+    const [currentRating, setCurrentRating] = useState(0);
+    const [clicked, setClicked] = useState(false);
+
+    useEffect(() => {
+      const fetchUserRating = async () => {
+        const res = await fetch(
+          `/api/users/${userId}/movies?fieldType=Rated&movieID=${movieImdbId}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await res.json();
+        setCurrentRating(data);
+      };
+
+      userId && movieImdbId && fetchUserRating();
+    }, [currentRating, userId, movieImdbId]);
+
+    useImperativeHandle(ref, () => ({
+      saveToDb,
+      resetRating,
+    }));
 
     const saveToDb = async () => {
-      if (currentRating != 0) {
+      console.log("saveToDb Movie:", movieImdbId, " Rating:", originalRating);
+      if (originalRating != 0) {
         await fetch(`/api/users/${userId}/movies?fieldType=Rated`, {
           method: "POST",
           headers: {
@@ -45,57 +60,48 @@ const Stars: FC<StarsProps> = ({
           },
           body: JSON.stringify({
             movieImdbId: movieImdbId,
-            rating: currentRating,
+            rating: originalRating,
           }),
         });
       }
     };
 
-    userId && movieImdbId && fetchUserRating();
-
-    if (save) {
-      console.log("Saving to db in Stars");
-      saveToDb();
-    }
-  }, [save, currentRating, userId, movieImdbId]);
-
-  // Reset rating when reset is true
-  useEffect(() => {
-    if (reset) {
+    const resetRating = () => {
       setCurrentRating(0);
-    }
-  }, [reset]);
+    };
+    const handleStarHover = (starIndex: number) => {
+      setCurrentRating(starIndex + 1);
+    };
 
-  const handleStarHover = (starIndex: number) => {
-    setCurrentRating(starIndex + 1);
-  };
+    const handleStarLeave = () => {
+      clicked ? setCurrentRating(currentRating) : setCurrentRating(0);
+    };
 
-  const handleStarLeave = () => {
-    clicked ? setCurrentRating(currentRating) : setCurrentRating(0);
-  };
+    return (
+      <div className="flex items-center hover:cursor-pointer">
+        {[...Array(10)].map((_, index) => (
+          <Icon
+            key={index}
+            icon={"tabler:star"}
+            width={size}
+            height={size}
+            onClick={() => {
+              setOriginalRating(index + 1);
+            }}
+            onMouseEnter={() => handleStarHover(index)}
+            onMouseLeave={handleStarLeave}
+            className={cn(
+              index < currentRating || index < originalRating
+                ? " fill-yellow-200  text-yellow-300"
+                : " text-gray-500"
+            )}
+          />
+        ))}
+      </div>
+    );
+  }
+);
 
-  return (
-    <div className="flex items-center hover:cursor-pointer">
-      {[...Array(10)].map((_, index) => (
-        <Icon
-          key={index}
-          icon={"tabler:star"}
-          width={size}
-          height={size}
-          onClick={() => {
-            setOriginalRating(index + 1);
-          }}
-          onMouseEnter={() => handleStarHover(index)}
-          onMouseLeave={handleStarLeave}
-          className={cn(
-            index < currentRating || index < originalRating
-              ? " fill-yellow-200  text-yellow-300"
-              : " text-gray-500"
-          )}
-        />
-      ))}
-    </div>
-  );
-};
+Stars.displayName = "Stars";
 
 export default Stars;
