4407769d323ad890636e7b4fc05b106feedfa854
Sebastian Munthe <sebasmu@stud.ntnu.no>
2024-02-28 21:09:24 +0100

    Forgot to account for a nothing-search, now it is accounted for

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,29 +1,23 @@
 "use client";
 
-import MovieScrollArea from "../../../components/MovieScrollArea";
+import MovieScrollArea from "../../components/MovieScrollArea";
 
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 import { User as FirebaseUser } from "firebase/auth";
-import { Movie } from "../../../types/Movie";
+import { Movie } from "../../types/Movie";
 import { auth } from "@/firebase/config";
-import { usePathname } from 'next/navigation'
-
 
 export default function Search() {
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<FirebaseUser | null>(null);
   const [notLoggedIn, setNotLoggedIn] = useState(false);
   const [movies, setMovies] = useState<Movie[]>();
-  const path = usePathname().split("/");
-  const sw = path[path.length-1];
-  const search = "../api/search?sw=" + sw;
 
   useEffect(() => {
     const fetchSearched = async () => {
-      console.log(search)
 
-      const res = await fetch(search, {
+      const res = await fetch("../api/search?sw=", {
         method: "GET",
       });
       const data = await res.json();
@@ -57,7 +51,7 @@ export default function Search() {
         <div>
           <div className="relative">
           <section className="backdrop-blur-sm bg-slate-950/30">
-            <MovieScrollArea title={"Results for " + sw + "..."} movies={movies} />
+            <MovieScrollArea title={"Results for ..."} movies={movies} />
           </section>
         </div>
         </div>
