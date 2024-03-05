20f40a13a8be1da34af6849a0ea3c569b004ed1c
niklasu <niklasu@smint.no>
2024-03-05 14:17:50 +0100

    Worked on gridview for bross

--- app/(main)/browse/page.tsx
+++ app/(main)/browse/page.tsx
@@ -1,9 +1,25 @@
-const Browse = () => {
+"use client";
+
+import {useState} from "react";
+import {User as FirebaseUser} from "@firebase/auth";
+import MovieCard from "./organisms/MovieCard";
+import cn from "classnames";
+import PopUp from "./organisms/PopUp";
+import Stars, { StarsRef } from "./atoms/Stars";
+import { Movie } from "../types/Movie";
+import { auth } from "@/firebase/config";
+
+export default function Browse(){
+
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState<FirebaseUser | null>(null);
+    const [notLoggedIn, setNotLoggedIn] = useState(false);
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+
     return(
         <div>
             Browse
         </div>
     )
-    }
-
-export default Browse
+}
