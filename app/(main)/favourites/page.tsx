af191ca6b238c93bd8bef048a59418eb19058ef6
lassedi <lassedi@stud.ntnu.no>
2024-03-12 13:44:17 +0100

    Changed scrollareas to actors and genres

--- app/(main)/favourites/page.tsx
+++ app/(main)/favourites/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import MovieScrollArea from "@/app/components/MovieScrollArea";
+import { Actor } from "@/app/types/Actor";
+import { Genre } from "@/app/types/Genre";
 import { Movie } from "@/app/types/Movie";
 import { auth } from "@/firebase/config";
 // db can be imported from here
 import { useEffect, useState } from "react";
 
 const Favourites = () => {
-  const [moviesWatched, setMoviesWatched] = useState<Movie[]>([]);
-  const [moviesRated, setMoviesRated] = useState<Movie[]>([]);
+  const [moviesWatched, setMoviesWatched] = useState<Genre[]>([]);
+  const [moviesRated, setMoviesRated] = useState<Actor[]>([]);
   const [userId, setUserId] = useState<string | null>(null);
 
   useEffect(() => {
@@ -20,7 +22,7 @@ const Favourites = () => {
 
     const fecthUserWacthedMovies = async () => {
       if (userId === null) return;
-      const res = await fetch(`/api/users/${userId}/movies?fieldType=Watched`, {
+      const res = await fetch(`/api/users/${userId}/genres`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -32,7 +34,7 @@ const Favourites = () => {
 
     const fecthUserRatedMovies = async () => {
       if (userId === null) return;
-      const res = await fetch(`/api/users/${userId}/movies?fieldType=Rated`, {
+      const res = await fetch(`/api/users/${userId}/actors?fieldType=actorsLiked`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -49,10 +51,10 @@ const Favourites = () => {
       <div className=" flex justify-center">
       <div className=" w-4/5 flex justify-around">
         <div className="w-2/5">
-          <MovieScrollArea title="Favourite genres:" movies={moviesWatched} genres={[]} actors={[]}/>
+          <MovieScrollArea title="Favourite genres:" movies={[]} genres={moviesWatched} actors={[]}/>
         </div>
         <div className="w-2/5 ">
-          <MovieScrollArea title="Favourite actors:" movies={moviesRated} genres={[]} actors={[]}/>
+          <MovieScrollArea title="Favourite actors:" movies={[]} genres={[]} actors={moviesRated}/>
         </div>
       </div>
       </div>
