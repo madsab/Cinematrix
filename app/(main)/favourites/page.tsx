6387da9a87bb6b5c6d8834d3682fd8f06d367052
lassedickhausen <lasse.dickhausen@lyse.net>
2024-03-17 03:10:33 +0100

    Clean-up from new component, DEL old imports, slight visual tweak

--- app/(main)/favourites/page.tsx
+++ app/(main)/favourites/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import Image from "@/node_modules/next/image"
+
 import MovieScrollArea from "@/app/components/MovieScrollArea";
+import ProfileBanner from "@/app/components/ProfileBanner";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Actor } from "@/app/types/Actor";
 import { Genre } from "@/app/types/Genre";
 import { Movie } from "@/app/types/Movie";
 import { auth } from "@/firebase/config";
-import Avatar from "../../assets/images/avatar.png";
-import Poster from "../../assets/images/profile_banner.jpg";
+
 // db can be imported from here
 import { useEffect, useState } from "react";
 
@@ -97,31 +97,21 @@ const Favourites = () => {
 
   return (
     <div className="">
-      <div className="relative container mx-auto mt-3 flex justify-content z-0 h-[20rem] w-full ">
-        <Image src={Poster} alt="Profile Poster" className="h-full w-full object-cover " />
-      </div>
-      
-      
-
-      <div className="-mt-[1%] backdrop-blur-[1px] min-h-40 flex items-center justify-center ">
-        <Image src={Avatar} width={90} height={90} alt={"Avatar"}/>
-        <p className="flex ml-10">{upperUsername ? `${upperUsername}'s favourites` : 'Favourites'}</p>
-      </div>
-      
+      <ProfileBanner username={upperUsername}/>
       <div className="flex space-x-10">
           <div className="w-1/2">
-            <MovieScrollArea className="bg-gradient-to-t from-black via-pink-950 to-black" title={<span className="flex items-center">Favourite genres &nbsp;<Icon icon="tabler:globe-filled"/> </span>} movies={[]} genres={favouriteGenres} actors={[]}/>
+            <MovieScrollArea className="bg-gradient-to-t from-black from-20% via-pink-950 to-black to-80%" title={<span className="flex items-center">Favourite genres &nbsp;<Icon icon="tabler:globe-filled"/> </span>} movies={[]} genres={favouriteGenres} actors={[]}/>
           </div>
           <div className="w-1/2">
-            <MovieScrollArea className="bg-gradient-to-t from-black via-pink-950 to-black" title={<span className="flex items-center">Favourite actors &nbsp;<Icon icon="tabler:user-heart"/> </span>} movies={[]} genres={[]} actors={likedActors}/>
+            <MovieScrollArea className="bg-gradient-to-t from-black from-20% via-pink-950 to-black to-80%" title={<span className="flex items-center">Favourite actors &nbsp;<Icon icon="tabler:user-heart"/> </span>} movies={[]} genres={[]} actors={likedActors}/>
           </div>
       </div>
       <div className=" flex space-x-10">
         <div className="w-1/2">
-          <MovieScrollArea className="bg-gradient-to-t from-black via-pink-950 to-black" title={<span className="flex items-center">Favourite movies &nbsp;<Icon icon="tabler:movie"/> </span>} movies={likedMovies} genres={[]} actors={[]}/>
+          <MovieScrollArea className="bg-gradient-to-t from-black from-20% via-pink-950 to-black to-80%" title={<span className="flex items-center">Favourite movies &nbsp;<Icon icon="tabler:movie"/> </span>} movies={likedMovies} genres={[]} actors={[]}/>
         </div>
         <div className="w-1/2">
-          <MovieScrollArea className="bg-gradient-to-t from-black via-pink-950 to-black" title={<span className="flex items-center">Favourite directors &nbsp;<Icon icon="game-icons:director-chair"/> </span>} movies={[]} genres={[]} actors={likedDirectors}/>
+          <MovieScrollArea className="bg-gradient-to-t from-black from-20% via-pink-950 to-black to-80%" title={<span className="flex items-center">Favourite directors &nbsp;<Icon icon="game-icons:director-chair"/> </span>} movies={[]} genres={[]} actors={likedDirectors}/>
         </div>
       </div>
 
