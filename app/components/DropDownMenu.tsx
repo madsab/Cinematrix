0268af4940f5a84c2d376976394b882df1b2da78
madsab <mads.barnes@hotmail.com>
2024-03-19 14:28:54 +0100

    mod: map trough all genres dynamicaly

--- app/components/DropDownMenu.tsx
+++ app/components/DropDownMenu.tsx
@@ -1,46 +1,47 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Icon } from "@iconify/react/dist/iconify.js";
-import {Genre} from "@/app/types/Genre";
-
+import { Genre } from "@/app/types/Genre";
 
 interface DropDownMenuProps {
-    genres: Genre[];
-    Title: string;
-    current: string
-    setCurrent: any
+  genres: Genre[];
+  Title: string;
+  current: string;
+  setCurrent: any;
 }
 
 const DropDownMenu: React.FC<DropDownMenuProps> = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [ButtonText, setButtonText] = useState<string>(props.Title);
-    const refDropdown = useRef<any>(null);
-    const refButton = useRef<any>(null);
-    const icon = isOpen ? "tabler:arrow-big-up-filled" : "tabler:arrow-big-down";
-    const [resetFilter, setResetFilter] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [ButtonText, setButtonText] = useState<string>(props.Title);
+  const refDropdown = useRef<any>(null);
+  const refButton = useRef<any>(null);
+  const icon = isOpen ? "tabler:arrow-big-up-filled" : "tabler:arrow-big-down";
+  const [resetFilter, setResetFilter] = useState(false);
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleItemClick = (item: string) => {
-        if (item != props.Title) {
-            setResetFilter(true);
-            props.setCurrent(item);
-        }
-        else {
-          setResetFilter(false)
-          props.setCurrent('')
-        }
-        console.log(`Selected: ${item}`);
-        setButtonText(item);
-        setIsOpen(false);
-    };
-
-
+  const handleItemClick = (item: string) => {
+    if (item != props.Title) {
+      setResetFilter(true);
+      props.setCurrent(item);
+    } else {
+      setResetFilter(false);
+      props.setCurrent("");
+    }
+    console.log(`Selected: ${item}`);
+    setButtonText(item);
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (isOpen && refDropdown.current && !refDropdown.current.contains(event.target) && !refButton.current.contains(event.target)) {
+      if (
+        isOpen &&
+        refDropdown.current &&
+        !refDropdown.current.contains(event.target) &&
+        !refButton.current.contains(event.target)
+      ) {
         setIsOpen(false);
       }
     };
@@ -51,49 +52,53 @@ const DropDownMenu: React.FC<DropDownMenuProps> = (props) => {
     };
   }, [refDropdown, isOpen]);
 
-
-
   return (
-        <div className="dropdown mr-16">
-          <button ref={refButton} onClick={toggleDropdown}
+    <div className="dropdown mr-16">
+      <button
+        ref={refButton}
+        onClick={toggleDropdown}
+        className="dropdown-toggle px-4 py-3 text-Button flex justify-between items-center  gap-3 w-44  bg-gradient-to-r from-black to-grape"
+        style={{
+          border: "2px solid #FFF",
+          borderRadius: "5px",
+        }}
+      >
+        <div>{ButtonText}</div>
+        <Icon icon={icon}></Icon>
+      </button>
 
-                  className="dropdown-toggle px-4 py-3 text-Button flex justify-between items-center  gap-3 w-44  bg-gradient-to-r from-black to-grape"
-                  style={{
-                    border: "2px solid #FFF",
-                    borderRadius: "5px",
-                  }}>
-            <div>{ButtonText}</div>
-            <Icon icon={icon}>
-
-            </Icon>
-          </button>
-
-          {isOpen && (
-              <div ref={refDropdown} className="dropdown-menu mt-2  w-44 bg-gradient-to-r from-black to-grape " style={{
-                position: "absolute",
-                zIndex: "1",
-                border: "2px solid #FFF",
-                cursor: "pointer",
-              }}>
-                {resetFilter && (<div className="hover:bg-white hover:text-night p-2.5 border-white border-solid border-b-2 italic"
-                     onClick={() => handleItemClick(props.Title)} >Reset Filter
-                </div>)}
-                <div className="hover:bg-white hover:text-night p-2.5 "
-                     onClick={() => handleItemClick("Action")}>Action
-                </div>
-                <div className="hover:bg-white hover:text-night p-2.5"
-                     onClick={() => handleItemClick("Adventure")}>Adventure
-                </div>
-                <div className="hover:bg-white hover:text-night p-2.5"
-                     onClick={() => handleItemClick("Animation")}>Animation
-                </div>
-                <div className="hover:bg-white hover:text-night p-2.5"
-                     onClick={() => handleItemClick("Bad")}>Bad
-                </div>
-              </div>
+      {isOpen && (
+        <div
+          ref={refDropdown}
+          className="dropdown-menu mt-2  w-44 bg-gradient-to-r from-black to-grape "
+          style={{
+            position: "absolute",
+            zIndex: "1",
+            border: "2px solid #FFF",
+            cursor: "pointer",
+          }}
+        >
+          {resetFilter && (
+            <div
+              className="hover:bg-white hover:text-night p-2.5 border-white border-solid border-b-2 italic"
+              onClick={() => handleItemClick(props.Title)}
+            >
+              Reset Filter
+            </div>
           )}
+          {props.genres.map((genre, key) => (
+            <div
+              key={key}
+              className="hover:bg-white hover:text-night p-2.5"
+              onClick={() => handleItemClick(genre.id)}
+            >
+              {genre.id}
+            </div>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default DropDownMenu;
