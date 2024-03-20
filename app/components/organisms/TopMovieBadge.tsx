9c7bd18ff4fa550aaa3ccc61b9abce61bb8544fd
madsab <mads.barnes@hotmail.com>
2024-03-20 10:26:30 +0100

    fix: styling of number

--- app/components/organisms/TopMovieBadge.tsx
+++ app/components/organisms/TopMovieBadge.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 interface TopMovieBadgeProps {
   rank: number;
@@ -6,9 +6,9 @@ interface TopMovieBadgeProps {
 
 const TopMovieBadge: FC<TopMovieBadgeProps> = ({ rank }) => {
   return (
-    <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-20 h-20 flex items-center justify-center bg-black bg-opacity-75 rounded-full border-4 border-white">
-    <span className="text-white text-4xl font-bold">{rank}</span>
-  </div>
+    <div className="absolute top-1/2 left-6 transform -translate-x-1/2 -translate-y-1/2 z-10 w-16 h-16 flex items-center justify-center bg-black bg-opacity-75 rounded-full border-4 border-white">
+      <span className="text-white text-4xl font-bold">{rank}</span>
+    </div>
   );
 };
 
